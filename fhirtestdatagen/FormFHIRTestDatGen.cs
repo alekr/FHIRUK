cb53f2c718f6149d60c990d42a296fe29076b65d
Alek Radjenovic <alek.radjenovic@hscic.gov.uk>
2015-05-17 22:39:51 +0100

    Initial commit
    fhirres (FHIR resource library, resources:=[Patient]), fhirTest(scrap
    test), fhirtestdatagen (Test Data Generator)

---
+++ fhirtestdatagen/FormFHIRTestDatGen.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FHIRUK.Resources;
+
+namespace fhirtestdatagen
+{
+    public partial class FormFHIRTestDatGen : Form
+    {
+        private static Int32 PATIENT_COUNT = 10;
+
+        HumaneNameGenerator genNames = null;
+        AddressGenerator genAddresses = null;
+
+        public FormFHIRTestDatGen()
+        {
+            InitializeComponent();
+        }
+
+        private void butGenerate_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < PATIENT_COUNT; i++)
+            {
+                Patient patient = GeneratePatient();
+                AddPatientToList(patient);
+            }
+        }
+
+        private Patient GeneratePatient()
+        {
+            Patient patient = new Patient();
+
+            GenerateIdentifier(patient);
+            GenerateName(patient);
+            GenerateTelecom(patient);
+            GenerateDOB(patient);
+            GenerateAddress(patient);
+
+            return patient;
+        }
+
+        private void GenerateIdentifier(Patient patient)
+        {
+            patient.Identifier = IdentifierGenerator.GetRandomIdentifiers();
+        }
+
+        private void GenerateName(Patient patient)
+        {
+            List<String> given;
+            List<String> family;
+            EnumGender gender;
+
+            genNames.GetRandomName(out given, out family, out gender);
+
+            patient.Name = new HumanNames()
+            {
+                new HumanName()
+                {
+                    Given = given,
+                    Family = family
+                }
+            };
+
+            patient.Gender = gender;
+        }
+
+        private void GenerateTelecom(Patient patient)
+        {
+            String familyName = patient.Name[0].Family[0];
+            patient.Telecom = TelecomGenerator.GetRandomContacts(familyName);
+        }
+
+        private void GenerateDOB(Patient patient)
+        {
+            Random randomGenerator = new Random(Guid.NewGuid().GetHashCode());
+
+            Int32 year = randomGenerator.Next(1920, 2015);
+            Int32 month = randomGenerator.Next(0, 12) + 1;
+            Int32 day;
+            switch (month)
+            {
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    day = randomGenerator.Next(0, 30) + 1;
+                    break;
+                case 2:
+                    day = randomGenerator.Next(0, 28) + 1;
+                    break;
+                default:
+                    day = randomGenerator.Next(0, 31) + 1;
+                    break;
+            }
+
+            patient.BirthDate = new DateTime(year, month, day);
+        }
+
+        private void GenerateAddress(Patient patient)
+        {
+            patient.Address = genAddresses.GetRandomAddresses(EnumAddressUse.home); 
+        }
+
+        private void AddPatientToList(Patient patient)
+        {
+            if ((patient == null) || (patient.Identifier == null) || (patient.Identifier.Count < 1))
+                return;
+
+            ListViewItem item = listViewData.Items.Add(patient.Identifier.ToString());
+            item.SubItems.Add(patient.Name.ToString());
+            item.SubItems.Add(patient.Telecom.ToString());
+            item.SubItems.Add(patient.Gender.ToString());
+            item.SubItems.Add(patient.BirthDate.ToShortDateString());
+            item.SubItems.Add(String.Empty);    // deceased
+            item.SubItems.Add(patient.Address.ToString());
+
+            item.Tag = patient;
+        }
+
+        private void FormFHIRTestDatGen_Load(object sender, EventArgs e)
+        {
+            genNames = new HumaneNameGenerator();
+            genAddresses = new AddressGenerator();
+        }
+
+        private void listViewData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewData.SelectedItems.Count == 0)
+                textBox1.Text = String.Empty;
+            else
+            {
+                ListViewItem item = listViewData.SelectedItems[0];
+                Patient patient = item.Tag as Patient;
+                textBox1.Text = patient.JSON();
+            }
+        }
+    }
+}
---
+++ fhirtestdatagen/gen_id.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FHIRUK.Resources;
+
+namespace fhirtestdatagen
+{
+    public class IdentifierGenerator
+    {
+        public static Identifiers GetRandomIdentifiers()
+        {
+            Random randomGenerator = new Random(Guid.NewGuid().GetHashCode());
+
+            Identifiers ids = new Identifiers();
+            Identifier id;
+
+            // EnumIdentifierUse = Undefined, Usual, Official, Temp, Secondary
+            // generate at least one ID 99% of the time, and no ID 1% of the time
+            if (randomGenerator.Next(0, 99) > 0)
+            {
+                // generate Official 99% of the time
+                if (randomGenerator.Next(0, 99) == 0)
+                {
+                    // no ID generated
+                }
+                else
+                {
+                    // generating offical (NHS number) ID
+                    Int32 uniqueID = randomGenerator.Next(0, 10000000);
+                    id = new Identifier()
+                    {
+                        Use = EnumIdentifierUse.Official,
+                        System = new Uri("http://www.nhs.uk/NHSEngland/thenhs/records/Pages/thenhsnumber.aspx"),
+                        Value = "999" + uniqueID.ToString("D7")
+                    };
+                    ids.Add(id);
+
+                    // generate an additional ID 20% of time
+                    if (randomGenerator.Next(0, 100) < 20)
+                    {
+                        EnumIdentifierUse use;
+                        do
+                        {
+                            // need 1, 3 or 4 to map to Usual, Temp or Secondary
+                            use = (EnumIdentifierUse)(randomGenerator.Next(0, 4) + 1);
+                        } while (use == EnumIdentifierUse.Official);
+
+                        id = new Identifier()
+                        {
+                            Use = use,
+                            Value = randomGenerator.Next(0,1000000).ToString("D6") //  generate a 6 digit number
+                        };
+                        ids.Add(id);
+                    }
+                }
+            }
+
+            return ids;
+        }
+    }
+}
